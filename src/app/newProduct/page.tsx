a6b4b0838bd4f81e5aff7f4b230a54b251efa7f6
Marcus <devsilvamarcus@gmail.com>
2025-07-21 21:21:16 -0300

    feat: improve "Voltar para produtos" button style

--- src/app/newProduct/page.tsx
+++ src/app/newProduct/page.tsx
@@ -1,21 +1,22 @@
 import ProductForm from "@/components/ProductForm";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
 export default function CreateProduct() {
   return (
     <>
-      <div className="my-4 flex items-center gap-2 transition-colors hover:opacity-75">
-        <ArrowLeft className="h-4 w-4" />
-        <Link href="/">Voltar para produtos</Link>
-      </div>
+      <Button
+        variant="ghost"
+        className="my-4 transition-colors hover:opacity-75"
+      >
+        <Link href="/" className="flex items-center gap-2">
+          <ArrowLeft className="h-4 w-4" />
+          Voltar para produtos
+        </Link>
+      </Button>
 
-      <ProductForm
-        title="Criar Novo Produto
-"
-      />
+      <ProductForm title="Criar Novo Produto" />
     </>
   );
 }
