094f2191aa897890ef53c7dfe41222683c36e731
Marcus Silva <marcus.silva@forpeople.io>
2025-07-22 11:52:32 -0300

    fix: allow all image domains for Next.js Image component

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
+       {
         protocol: "https",
-        hostname: "fakestoreapi.com",
+        hostname: "**",
         port: "",
-        pathname: "/img/**",
-      },
+        pathname: "/**",
+      }
     ],
   },
 };
